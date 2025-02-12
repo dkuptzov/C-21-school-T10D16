3675dfb1c72af3ff93fd099f9dd9e6039af143c6
dkuptzov <101925838+dkuptzov@users.noreply.github.com>
2025-02-12 20:16:38 +0300

    Update s21_string.h

--- s21_string.h
+++ s21_string.h
@@ -1,9 +1,11 @@
 #ifndef S21_STRING_H
 #define S21_STRING_H
 
-size_t s21_strlen(const char *str);
+int s21_strlen(const char *str);
 int s21_strcmp(const char *str1, const char *str2);
 int s21_strcpy(char *str1, const char *str2);
-//char *s21_strcat(char *str1, const char *str2);
 int s21_strcat(char *str1, const char *str2);
+int s21_strchr(const char *str1, const char *symbol);
+int s21_strstr(const char *str1, const char *str2);
+
 #endif
